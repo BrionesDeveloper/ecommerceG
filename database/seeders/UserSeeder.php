ed146c02c8b6a61fde285eb297a2f7effb1ec1c1
DESKTOP-1N473R6\Omar Jr <omarbriones1821@gmail.com>
2022-10-17 02:33:28 -0500

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create(['name' => 'admin']);
+
+        User::create([
+            'name' => 'Omar Belzahet Briones Hernandez',
+            'email' => 'omar@guapissimaonline.com',
+            'password' => bcrypt('12345678')
+        ])->assignRole('admin');
+
+
+        User::factory(100)->create();
+    }
+}
