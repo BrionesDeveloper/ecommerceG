81299f3dab84d791479d2d2c08efbbb6b120e3c1
DESKTOP-1N473R6\Omar Jr <omarbriones1821@gmail.com>
2022-11-16 16:33:50 -0600

    FIX WELCOME

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,4 @@
 <x-app-layout>
     @livewire('welcome', ['categories' => $categories])
 
-    {{-- @foreach ($testarray as $item)
-    <div>
-
-        {{$item['name']}}
-    </div> --}}
-        
-    @endforeach
 </x-app-layout>
